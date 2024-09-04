422c6164402307ed646689a087691814c890b34d
Aleksei Tokarev <nonezonyx@gmail.com>
2024-09-04 17:25:13 +0300

    added tests and debug memory access

--- include/chip8_engine.hpp
+++ include/chip8_engine.hpp
@@ -9,15 +9,23 @@
 namespace nznyx::chip8 {
 class chip8_engine {
 public:
-    constexpr static std::size_t FONT_POSITION = 0x050;
-    constexpr static std::size_t PROGRAM_POSITION = 0x200;
+    chip8_engine();
 
     void change_fontset(const unsigned char fontset[80]) noexcept;
 
+    /* DEBUG FUNCTIONS */
+    const unsigned char *get_memory() const noexcept;
+
+    /* MEMORY LAYOUT CONSTANTS */
+    constexpr static std::size_t FONT_POSITION = 0x50;
+    constexpr static std::size_t PROGRAM_POSITION = 0x200;
+    constexpr static std::size_t MEMORY_SIZE = 4096;
+    constexpr static std::size_t VARIABLE_REGISTER_SIZE = 16;
+
 private:
     std::stack<unsigned char> stack;
-    unsigned char memory_[4096] = {0};
-    unsigned char V[16] = {0};
+    unsigned char memory_[MEMORY_SIZE] = {0};
+    unsigned char V[VARIABLE_REGISTER_SIZE] = {0};
     unsigned short PC = PROGRAM_POSITION;
     unsigned short I = 0;
     unsigned char delay_timer = 0;
--- src/chip8_engine.cpp
+++ src/chip8_engine.cpp
@@ -5,8 +5,16 @@
 
 namespace nznyx::chip8 {
 
+chip8_engine::chip8_engine() {
+    change_fontset(chip8_default_fontset);
+}
+
 void chip8_engine::change_fontset(const unsigned char fontset[80]) noexcept {
     std::memcpy(memory_ + FONT_POSITION, fontset, 80);
+}
+
+const unsigned char *chip8_engine::get_memory() const noexcept {
+    return memory_;
 }
 
 }  // namespace nznyx::chip8
