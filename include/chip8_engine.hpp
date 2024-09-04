d6b4be4d4ba0e6154a276f96034f505a912166e5
Aleksei Tokarev <nonezonyx@gmail.com>
2024-09-04 12:38:09 +0300

    created basic structure

---
+++ include/chip8_engine.hpp
@@ -0,0 +1,8 @@
+#ifndef CHIP8_ENGINE_HPP_
+#define CHIP8_ENGINE_HPP_
+
+namespace nznyx::chip8 {
+class chip8_engine {};
+}  // namespace nznyx::chip8
+
+#endif  // CHIP8_ENGINE_HPP_
---
+++ src/chip8_engine.cpp
@@ -0,0 +1,1 @@
+#include <chip8_engine>
