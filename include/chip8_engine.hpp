9edba75d87d9cb43840eed877d143621127d99f4
Aleksei Tokarev <nonezonyx@gmail.com>
2024-09-04 14:20:16 +0300

    added fwd for chip8endine, created basic CMakeLists

--- include/chip8_engine.hpp
+++ include/chip8_engine.hpp
@@ -1,8 +1,14 @@
 #ifndef CHIP8_ENGINE_HPP_
 #define CHIP8_ENGINE_HPP_
 
+#include <chip8_engine_fwd>
+
 namespace nznyx::chip8 {
-class chip8_engine {};
+class chip8_engine {
+private:
+public:
+    void load_fontset() noexcept;
+};
 }  // namespace nznyx::chip8
 
 #endif  // CHIP8_ENGINE_HPP_
