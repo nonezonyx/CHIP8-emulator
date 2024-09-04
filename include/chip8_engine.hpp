3f852f778f8a4acb725595168a0f2a1bce807304
Aleksei Tokarev <nonezonyx@gmail.com>
2024-09-04 16:15:05 +0300

    added basic memory layout, implemented change fontset function

--- include/chip8_engine.hpp
+++ include/chip8_engine.hpp
@@ -2,14 +2,26 @@
 #define CHIP8_ENGINE_HPP_
 
 #include <chip8_engine_fwd.hpp>
+#include <cstddef>
+#include <cwchar>
+#include <stack>
 
 namespace nznyx::chip8 {
 class chip8_engine {
-private:
-    unsigned char memory[4096];
-
 public:
-    void load_fontset() noexcept;
+    constexpr static std::size_t FONT_POSITION = 0x050;
+    constexpr static std::size_t PROGRAM_POSITION = 0x200;
+
+    void change_fontset(const unsigned char fontset[80]) noexcept;
+
+private:
+    std::stack<unsigned char> stack;
+    unsigned char memory_[4096] = {0};
+    unsigned char V[16] = {0};
+    unsigned short PC = PROGRAM_POSITION;
+    unsigned short I = 0;
+    unsigned char delay_timer = 0;
+    unsigned char sound_timer = 0;
 };
 }  // namespace nznyx::chip8
 
--- src/chip8_engine.cpp
+++ src/chip8_engine.cpp
@@ -1,1 +1,12 @@
+#include <cassert>
 #include <chip8_engine.hpp>
+#include <chip8_engine_fwd.hpp>
+#include <cstring>
+
+namespace nznyx::chip8 {
+
+void chip8_engine::change_fontset(const unsigned char fontset[80]) noexcept {
+    std::memcpy(memory_ + FONT_POSITION, fontset, 80);
+}
+
+}  // namespace nznyx::chip8
